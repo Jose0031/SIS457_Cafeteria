2f5bd85d4ffc03c4487b378b4f5ee5db467e3aba
Melbin Calle <Melbin243@gmail.com>
2023-11-05 10:33:22 -0400

    crud de cliente, venta y ventaDetalle en ClnCafeteria

---
+++ Sis457Cafeteria/ClnCafeteria_G4/VentaCln.cs
@@ -0,0 +1,82 @@
+﻿using CadCafeteria_G4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnCafeteria_G4
+{
+    public class VentaCln
+    {
+        public static int insertar(Venta venta)
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                context.Venta.Add(venta);
+                context.SaveChanges();
+                return venta.id;
+            }
+        }
+
+        public static int actualizar(Venta venta)
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                var existente = context.Venta.Find(venta.id);
+                existente.idCliente = venta.idCliente;
+                existente.totalVenta = venta.totalVenta;
+                existente.usuarioRegistro = venta.usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                var existente = context.Venta.Find(id);
+                existente.estado = -1;
+                existente.usuarioRegistro = usuarioRegistro;
+                return context.SaveChanges();
+
+            }
+        }
+
+        public static Venta get(int id)
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                return context.Venta.Find(id);
+
+            }
+        }
+
+        public static List<Venta> listar()
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                return context.Venta.Where(x => x.estado != -1).ToList();
+
+            }
+        }
+
+        public static List<paVentaListar_Result> listarPa()
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                return context.paVentaListar().ToList();
+
+            }
+        }
+
+        public static List<paVentaBuscar_Result> buscarPa(int parametro)
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                return context.paVentaBuscar(parametro).ToList();
+
+            }
+        }
+    }
+}
---
+++ Sis457Cafeteria/ClnCafeteria_G4/VentaDetalleCln.cs
@@ -0,0 +1,83 @@
+﻿using CadCafeteria_G4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnCafeteria_G4
+{
+    public class VentaDetalleCln
+    {
+        public static int insertar(VentaDetalle ventaDetalle)
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                context.VentaDetalle.Add(ventaDetalle);
+                context.SaveChanges();
+                return ventaDetalle.id;
+            }
+        }
+
+        public static int actualizar(VentaDetalle ventaDetalle)
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                var existente = context.VentaDetalle.Find(ventaDetalle.id);
+                existente.cantidad = ventaDetalle.cantidad;
+                existente.precioUnitario = ventaDetalle.precioUnitario;
+                existente.total = ventaDetalle.total;
+                existente.usuarioRegistro = ventaDetalle.usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                var existente = context.VentaDetalle.Find(id);
+                existente.estado = -1;
+                existente.usuarioRegistro = usuarioRegistro;
+                return context.SaveChanges();
+
+            }
+        }
+
+        public static VentaDetalle get(int id)
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                return context.VentaDetalle.Find(id);
+
+            }
+        }
+
+        public static List<VentaDetalle> listar()
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                return context.VentaDetalle.Where(x => x.estado != -1).ToList();
+
+            }
+        }
+
+        public static List<paVentaDetalleListar_Result> listarPa()
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                return context.paVentaDetalleListar().ToList();
+
+            }
+        }
+
+        public static List<paVentaDetalleBuscar_Result> buscarPa(int parametro)
+        {
+            using (var context = new LabCafeteria_G4Entities())
+            {
+                return context.paVentaDetalleBuscar(parametro).ToList();
+
+            }
+        }
+    }
+}
